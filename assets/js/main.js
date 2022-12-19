1395208c04b9513eb930c4c91ffe55558548085e
klebermkardel <molinakardel@gmail.com>
2022-12-19 18:18:10 -0300

    Cria ação de botão no qual ao clicar carrega mais pokémons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,35 @@
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreBtn = document.getElementById('loadMoreBtn')
+const limit = 5
+let offset = 0
 
 
 
-function convertPokemonToLi(pokemon) {
-    return `
-        <li class="pokemon ${pokemon.type}">
-            <span class="number">#${pokemon.number}</span>
-            <span class="name">${pokemon.name}</span>
 
-            <div class="detail">
-                <ol class="types">
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map((pokemon) =>  `
+            <li class="pokemon ${pokemon.type}">
+                <span class="number">#${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
+
+                <div class="detail">
+                    <ol class="types">
                     ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                </ol>
+                    </ol>
 
-                <img src="${pokemon.photo}"
-                alt="${pokemon.name}">
-            </div>
-        </li>
-    `
+                    <img src="${pokemon.photo}"
+                    alt="${pokemon.name}">
+                </div>
+            </li>
+    `).join('')
+        pokemonList.innerHTML += newHtml
+    })
 }
 
-const pokemonList = document.getElementById('pokemonList')
+loadPokemonItens(offset, limit)
 
-pokeApi.getPokemons().then((pokemons = []) => {
-    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+loadMoreBtn.addEventListener('click', () => {
+    offset += limit
+    loadPokemonItens(offset, limit)
 })
