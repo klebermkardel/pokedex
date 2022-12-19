8ddc86dc259b02bc92ed179a86bf06d2d7da0812
klebermkardel <molinakardel@gmail.com>
2022-12-19 14:39:09 -0300

    Integrando com a PokeAPI - Separa o consumo da API da manipulação de HTML

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,13 +25,12 @@ function convertPokemonToLi(pokemon) {
 
 const pokemonList = document.getElementById('pokemonList')
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => {
+pokeApi.getPokemons().then((pokemons) => {
+    const listItems = []
+
         for (let i = 0; i < pokemons.length; i++) {
             const pokemon = pokemons[i];
-            pokemonList.innerHTML += convertPokemonToLi(pokemon)
+            listItems.push(convertPokemonToLi(pokemon))
         }
+
     })
-    .catch((error) => console.error(error))
